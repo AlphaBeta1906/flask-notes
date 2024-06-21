1aa5354d1710eef391338ff6e0ceca228fd5ffec
alfarizi <farizi1906@gmail.com>
2024-06-21 09:17:23 +0700

    change config into python file

--- app.py
+++ app.py
@@ -4,23 +4,21 @@ from dotenv import load_dotenv , dotenv_values
 from models import db,User
 from controller.main import main
 from controller.auth import auth
-import os
 
 app = Flask(__name__)
 migrate = Migrate()
 
 def create_app():
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-
+    from config import Config
+    config = dotenv_values()
+    app.config.from_object(Config)
+    
     db.init_app(app)
     migrate.init_app(app,db,render_as_batch=True)
-
+    
     app.register_blueprint(main)
     app.register_blueprint(auth)
 
     return app
 
 app = create_app()
-
-app.run(debug=True)
